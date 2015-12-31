8f6de00af80a7039852ff273a3a38b8125bb6da5
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:55:42 +1300

    Converted to PSR-2

--- code/GoogleAnalyticsExtension.php
+++ code/GoogleAnalyticsExtension.php
@@ -1,12 +1,14 @@
 <?php
 
-class GoogleAnalyticsExtension extends Extension {
+class GoogleAnalyticsExtension extends Extension
+{
 
-	public function onAfterInit() {
-		if (Director::isLive()) {
-			$accountId = Config::inst()->get(get_class(), 'account_id');
-			if ($accountId) {
-				 Requirements::customScript(<<<JS
+    public function onAfterInit()
+    {
+        if (Director::isLive()) {
+            $accountId = Config::inst()->get(get_class(), 'account_id');
+            if ($accountId) {
+                Requirements::customScript(<<<JS
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -15,9 +17,8 @@ class GoogleAnalyticsExtension extends Extension {
 	ga('create', '$accountId', 'auto');
 	ga('send', 'pageview');
 JS
-				);
-			}
-		}
-	}
-
+                );
+            }
+        }
+    }
 }
